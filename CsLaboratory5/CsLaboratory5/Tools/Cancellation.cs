5a403b1a84ecd18400d631e23da6a510ce6e37fd
AlexFry123 <45427927+AlexFry123@users.noreply.github.com>
2019-04-05 21:51:55 +0300

    Update Cancellation.cs

--- CsLaboratory5/CsLaboratory5/Tools/Cancellation.cs
+++ CsLaboratory5/CsLaboratory5/Tools/Cancellation.cs
@@ -4,9 +4,9 @@ namespace CsLaboratory5.Tools
 {
     internal static class Cancellation
     {
-        public static CancellationTokenSource RefreshListCancellation { get; set; } = new CancellationTokenSource();
-        public static CancellationToken RefreshListToken { get; set; } = RefreshListCancellation.Token;
-        public static CancellationTokenSource RefreshMetaDataCancellation { get; set; } = new CancellationTokenSource();
-        public static CancellationToken RefreshMetaDataToken { get; set; } = RefreshMetaDataCancellation.Token;
+        internal static CancellationTokenSource RefreshListCancellation { get; set; } = new CancellationTokenSource();
+        internal static CancellationToken RefreshListToken { get; set; } = RefreshListCancellation.Token;
+        internal static CancellationTokenSource RefreshMetaDataCancellation { get; set; } = new CancellationTokenSource();
+        internal static CancellationToken RefreshMetaDataToken { get; set; } = RefreshMetaDataCancellation.Token;
     }
 }
